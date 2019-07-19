0b94c260125fb57bf81a0f8a650c410334fe5ab2
Maksym Surzhynskyi <maksym.surzhynskyi@gmail.com>
2019-07-19 16:19:32 +0300

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,3 +1,9 @@
+# Copyright (C) 2019 Maksym Surzhynskyi
+# Licence: MIT License
+# Author: Maksym Surzhynskyi
+# Contact: maksym.surzhynskyi@gmail.com
+# version="0.1"
+
 """
 ``Spltr`` is a simple PyTorch-based data loader and splitter. 
 It may be used to load arrays and matrices or Pandas DataFrames 

c5c5271e980e4e558293bc8b02452e820bd496ba
Maksym Surzhynskyi <maksym.surzhynskyi@gmail.com>
2019-07-20 16:01:11 +0300

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,25 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="spltr",
+    version="0.3",
+    author="Maksym Surzhynskyi",
+    author_email="maksym.surzhynskyi@gmail.com",
+    description="A simple PyTorch-based data loader and splitter.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/maksymsur/Spltr",
+    packages=setuptools.find_packages(),
+    keywords = ['PyTorch', 'Data loader', 'Data splitter', 'DataLoader'],
+    python_requires='>=3.6',
+    classifiers=[
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Intended Audience :: Science/Research",        
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
